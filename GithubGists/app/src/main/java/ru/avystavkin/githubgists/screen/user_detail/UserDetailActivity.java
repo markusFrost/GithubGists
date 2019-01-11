d1465bf8fdffbf075e39c3b8201c763e8e734618
markusFrost <andreywystawkin@yandex.ru>
2019-01-11 22:00:14 +0300

    simple add activity

---
+++ GithubGists/app/src/main/java/ru/avystavkin/githubgists/screen/user_detail/UserDetailActivity.java
@@ -0,0 +1,12 @@
+package ru.avystavkin.githubgists.screen.user_detail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
