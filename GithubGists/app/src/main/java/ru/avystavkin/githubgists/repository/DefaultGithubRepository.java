df4dcea50bd3e1cee32cba16c60dc9da2b835044
markusFrost <andreywystawkin@yandex.ru>
2019-01-12 18:13:07 +0300

    add composer

--- GithubGists/app/src/main/java/ru/avystavkin/githubgists/repository/DefaultGithubRepository.java
+++ GithubGists/app/src/main/java/ru/avystavkin/githubgists/repository/DefaultGithubRepository.java
@@ -4,11 +4,10 @@ import java.util.List;
 
 import androidx.annotation.NonNull;
 import io.reactivex.Observable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
 import ru.avystavkin.githubgists.api.GithubService;
 import ru.avystavkin.githubgists.content.Gist;
 import ru.avystavkin.githubgists.content.GistHistory;
+import ru.avystavkin.githubgists.utils.RxUtils;
 
 public class DefaultGithubRepository implements GithubRepository {
 
@@ -21,32 +20,24 @@ public class DefaultGithubRepository implements GithubRepository {
     @NonNull
     @Override
     public Observable<List<Gist>> getGists() {
-        return mGithubService.gists()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
+        return mGithubService.gists().compose(RxUtils.async());
     }
 
     @NonNull
     @Override
     public Observable<Gist> getGistById(String id) {
-        return mGithubService.gist_detail(id)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
+        return mGithubService.gist_detail(id).compose(RxUtils.async());
     }
 
     @NonNull
     @Override
     public Observable<List<Gist>> getGistsByUserName(String name) {
-        return mGithubService.user_detail(name)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
+        return mGithubService.user_detail(name).compose(RxUtils.async());
     }
 
     @NonNull
     @Override
     public Observable<List<GistHistory>> getCommitsByGistId(String id) {
-        return mGithubService.gist_commits(id)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
+        return mGithubService.gist_commits(id).compose(RxUtils.async());
     }
 }
